6244b11fe1d24eb8e4e12ca66e7196871e97f7cc
borkar-shital <borkarshital@github.com>
2023-01-12 22:07:07 +0530

    assignment

---
+++ loops.js
@@ -0,0 +1,24 @@
+//print value from 1 to 100
+console.log ("print value from 1 to 100")
+var i = 1;
+while (i <= 100) {
+    console.log (i);
+    i++
+}
+
+// print 2's table
+console.log ("print 2's table")
+var i = 2;
+while (i <= 20) {
+    console.log (i);
+    i = i+2
+}
+
+//match the point and print steps from given number (60)
+console.log ("match the point and print steps from given number (60)")
+var num = prompt("enter your number")
+do {
+    console.log (num);
+    num++
+}
+while (num <= 60);
